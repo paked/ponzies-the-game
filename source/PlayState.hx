1986ed730f56d786153284ad6e9f8b3938114b0b
Harrison <harrison@theshoebridges.com>
2015-12-21 23:02:27 -0800

    Add text and positions

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -12,11 +12,32 @@ import flixel.util.FlxMath;
  */
 class PlayState extends FlxState
 {
+  var money:Float;
+  var moneyText:FlxText;
+  var stampCost:Float;
+  var stampCostText:FlxText;
+  var timeLeft:Float;
+  var timeLeftText:FlxText;
+
   /**
    * Function that is called up when to state is created to set it up.
    */
   override public function create():Void
   {
+    moneyText = new FlxText(0, 0, 240, "Money Left:", 18);
+    stampCostText = new FlxText(0, 100, 240, "Stamp Cost:", 18);
+    timeLeftText = new FlxText(0, 200, 240, "Time Left:", 18);
+
+    var envelopePile = new FlxSprite();
+    envelopePile.x = 500 - envelopePile.width;
+    envelopePile.y = 20;
+    envelopePile.loadGraphic("assets/images/envelope.png");
+
+    add(moneyText);
+    add(stampCostText);
+    add(timeLeftText);
+    add(envelopePile);
+
     super.create();
   }
 
